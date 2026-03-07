8b9e86363dba3c65b262acd00993da02b380f187
kooogle <627275300@qq.com>
2026-03-07 20:23:49 +0800

    use 2605

--- trader_signal.py
+++ trader_signal.py
@@ -19,10 +19,10 @@ TQ_PASSWORD = "zp123789"
 
 # 交易品种 - 天勤合约格式
 SYMBOLS = [
-    {"code": "TA2205", "name": "PTA", "exchange": "CZCE"},
-    {"code": "OI2205", "name": "菜籽油", "exchange": "CZCE"},
-    {"code": "P2205", "name": "棕榈油", "exchange": "DCE"},
-    {"code": "V2205", "name": "PVC", "exchange": "DCE"},
+    {"code": "TA2605", "name": "PTA", "exchange": "CZCE"},
+    {"code": "OI2605", "name": "菜籽油", "exchange": "CZCE"},
+    {"code": "P2605", "name": "棕榈油", "exchange": "DCE"},
+    {"code": "V2605", "name": "PVC", "exchange": "DCE"},
 ]
 
 # 飞书配置
